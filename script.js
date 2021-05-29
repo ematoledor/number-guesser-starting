3cc1e53705e559c8d8e48f3da590ef917da782fe
Emanuel Toledo <ematolr@gmail.com>
2021-05-29 16:44:45 -0500

    practice project codecademy

---
+++ script.js
@@ -0,0 +1,38 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+  targetRandom = Math.floor(Math.random()*10);
+
+  return targetRandom;
+};
+
+const compareGuesses = (user, computer, secret) => {
+  let userValue = Math.abs(target - user);
+  let computerValue = Math.abs(target - computer);
+
+  if (userValue < computerValue) {
+    return true;
+  } else if (userValue < computerValue) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const updateScore = (winner) => {
+  if (winner === 'human') {
+    humanScore += 1;
+  } else if (winner === 'computer') {
+    computerScore += 1;
+  } else {
+    return 'ERROR!';
+  }
+}
+
+const advanceRound = () => {
+  currentRoundNumber += 1;
+}
